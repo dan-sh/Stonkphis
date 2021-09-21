9d8835204e33fe97b9e0064121024baa016dc096
dan-sh <dan.sheppard@aquaq.co.uk>
2021-09-21 14:10:53 +0100

    install script for linux

---
+++ install_stockfish.sh
@@ -0,0 +1,18 @@
+homedir=${PWD}
+## Pull in the package
+wget https://stockfishchess.org/files/stockfish_14_linux_x64_bmi2.zip
+
+## Unzip the Package
+unzip -q stockfish_14_linux_x64_bmi2.zip
+
+## Change Directory 
+cd stockfish_14_linux_x64_bmi2/sf_14_src/src
+
+## Build the Package
+make help
+make net
+make build ARCH=x86-64-modern
+
+## Clean Up .zip files etc
+rm -rf ${homedir}/__MACOSX
+rm ${homedir}/stockfish_14_linux_x64_bmi2.zip
